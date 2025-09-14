28b751e5f29521aa91bac59208168371a249757a
Sauban <sauban.git@gmail.com>
2025-09-14 12:36:36 +0000

    userRouter: added post api to create user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,6 @@
 import { Router, type Request, type Response } from "express";
+import { prisma } from "../db/prisma.js";
+import argon2 from "argon2";
 
 const router = Router();
 
@@ -7,6 +9,43 @@ router.get("/", async (req: Request, res: Response) => {
   res.json({
     message: "User Info",
   });
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  const {
+    name,
+    email,
+    password,
+  }: { name: string; email: string; password: string } = req.body;
+
+  if (!name || !email || !password)
+    return res.status(400).json({ error: "Please send valid input!" });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    const passwordHash = await argon2.hash(password);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash,
+      },
+    });
+
+    return res.status(200).json({
+      user,
+      message: "Successful creating user!",
+    });
+  } else {
+    return res.status(401).json({
+      error: "Email already exist..",
+    });
+  }
 });
 
 export { router as userRouter };
